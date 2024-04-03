154f2b541078d1b73470ae29328c601ceca27211
gaspar <contatomatheusgaspar@gmail.com>
2024-04-03 11:56:15 -0300

    update: Project 5

---
+++ Projects/book-list/src/components/BookProgress.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from "vue"
+
+
+const props = defineProps(['books'])
+
+
+const booksRead = computed(() => {
+    return props.books.filter(book => book.isRead).length
+})
+
+const booksReadMessage = computed(() => {
+    return booksRead.value >= props.books.length ? 
+    'Congrats! You read all books!' :
+    `${booksRead.value} of ${props.books.length} read books`
+})
+
+
+</script>
+
+<template>
+
+    <div class="books-read">
+        <label for="progress">your progress</label>
+        <br>
+        <progress :value="booksRead" :max="books.length"></progress>
+        <p>{{ booksReadMessage }}</p>
+    </div>
+    
+</template>
